b1dc1916ff5e5dc1a72fbc58c11700cb8fd62327
HunkBenny <80793807+HunkBenny@users.noreply.github.com>
2022-08-17 21:55:36 +0200

    Add files via upload

---
+++ integration_prestashop/models/__init__.py
@@ -0,0 +1,10 @@
+#  See LICENSE file for full copyright and licensing details.
+
+from . import sale_order
+from . import product_template
+from . import product_product
+from . import product_public_category
+
+from . import sale_integration
+from . import fields
+from . import external
